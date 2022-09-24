49776a7c91d91f1f1171bb10f1a19fd1b352a17d
angelstakepool <60072039+angelstakepool@users.noreply.github.com>
2022-09-24 11:47:23 -0400

    Update blend.sh

--- blend.sh
+++ blend.sh
@@ -70,7 +70,7 @@ else
     j=$($cncli_path ping --host ${host} --port ${port})
     status=$(jq -r '.status' <<< $j)
     version=$(jq -r '.networkProtocolVersion' <<< $j)
-    if [[ $status == 'ok' ]] && [[ $version == 9 ]]; then
+    if [[ $status == 'ok' ]] && [[ $version -gt 8 ]]; then
       connectDuration=$(jq -r '.connectDurationMs' <<< $j)
       durationMs=$(jq -r '.durationMs' <<< $j)
       echo -e "   ${SUCCESS}Good Peer!${NC} Host: ${host} ConnectDurationMs: ${connectDuration} DurationMs: ${durationMs} ProtocolVersion: ${version}" 1>&2
